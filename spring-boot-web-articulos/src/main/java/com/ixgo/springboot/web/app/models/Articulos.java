35dd4377b07362a1d5c029bc8b2b42193ac9b5ae
ARI <ARI@192.168.1.13>
2020-05-10 15:42:33 -0500

    Formateo de código

--- spring-boot-web-articulos/src/main/java/com/ixgo/springboot/web/app/models/Articulos.java
+++ spring-boot-web-articulos/src/main/java/com/ixgo/springboot/web/app/models/Articulos.java
@@ -1,25 +1,13 @@
 package com.ixgo.springboot.web.app.models;
 
 public class Articulos {
-	
+
 	private String descripcion;
 	private float precio;
 	private String marca;
-	public String getDescripcion() {
-		return descripcion;
-	}
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
-	}
-	public float getPrecio() {
-		return precio;
-	}
-	public void setPrecio(float precio) {
-		this.precio = precio;
-	}
-	public String getMarca() {
-		return marca;
-	}
+	private int cantidad;
+	private String proveedor;
+
 	public Articulos(String descripcion, float precio, String marca, int cantidad, String proveedor) {
 		this.descripcion = descripcion;
 		this.precio = precio;
@@ -27,22 +15,45 @@ public class Articulos {
 		this.cantidad = cantidad;
 		this.proveedor = proveedor;
 	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public float getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
+
 	public int getCantidad() {
 		return cantidad;
 	}
+
 	public void setCantidad(int cantidad) {
 		this.cantidad = cantidad;
 	}
+
 	public String getProveedor() {
 		return proveedor;
 	}
+
 	public void setProveedor(String proveedor) {
 		this.proveedor = proveedor;
 	}
-	private int cantidad;
-	private String proveedor;
 
 }
