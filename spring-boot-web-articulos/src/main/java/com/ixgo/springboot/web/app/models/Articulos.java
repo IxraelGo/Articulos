3ee566e999797f11d2956d3ec58d8afa52fff9fc
Haziel-Kau <65087494+Haziel-Kau@users.noreply.github.com>
2020-05-10 15:37:46 -0600

    constructor_puesto

--- spring-boot-web-articulos/src/main/java/com/ixgo/springboot/web/app/models/Articulos.java
+++ spring-boot-web-articulos/src/main/java/com/ixgo/springboot/web/app/models/Articulos.java
@@ -5,8 +5,44 @@ public class Articulos {
 	private String descripcion;
 	private float precio;
 	private String marca;
-	private float cantidad;
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public Articulos(String descripcion, float precio, String marca, int cantidad, String proveedor) {
+		this.descripcion = descripcion;
+		this.precio = precio;
+		this.marca = marca;
+		this.cantidad = cantidad;
+		this.proveedor = proveedor;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public int getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	public String getProveedor() {
+		return proveedor;
+	}
+	public void setProveedor(String proveedor) {
+		this.proveedor = proveedor;
+	}
+	private int cantidad;
 	private String proveedor;
-	
 
 }
