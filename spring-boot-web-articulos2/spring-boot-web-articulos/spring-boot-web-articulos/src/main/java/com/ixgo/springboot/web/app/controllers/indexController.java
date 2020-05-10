792b68e49c0a64b8ca2728d364113390517d2915
ARI <ARI@192.168.1.13>
2020-05-10 00:42:05 -0500

    Prueba 2.1 2

---
+++ spring-boot-web-articulos2/spring-boot-web-articulos/spring-boot-web-articulos/src/main/java/com/ixgo/springboot/web/app/controllers/indexController.java
@@ -0,0 +1,20 @@
+package com.ixgo.springboot.web.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+@RequestMapping ("/app")
+public class indexController {
+
+	@GetMapping("/index")
+	public String index(Model model) {
+		model.addAttribute("titulo","Articulos-Inicio");
+		return "index";
+	}
+	
+}
+	
---
+++ spring-boot-web-articulos2/spring-boot-web-articulos/src/main/java/com/ixgo/springboot/web/app/controllers/indexController.java
@@ -0,0 +1,18 @@
+package com.ixgo.springboot.web.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping ("/app")
+
+public class indexController {
+
+	@GetMapping("/index")
+	public String index(Model model) {
+		return "index";
+	}
+}
+
